7bc8af25e604a9a53b74ac29143955e673660090
Rakshit <rakshit.jod@gmail.com>
2026-07-22 18:53:41 +0530

    Sprint 3 - Added Material ripple animation

--- Casio Pro calculator/script.js
+++ Casio Pro calculator/script.js
@@ -233,3 +233,40 @@ if (clearHistoryBtn) {
 
 loadHistory();
 renderHistory();
+
+// ======================================
+// Button Ripple Effect
+// ======================================
+
+document.querySelectorAll(".btn").forEach(button => {
+
+    button.addEventListener("click", function(e){
+
+        const ripple = document.createElement("span");
+
+        ripple.classList.add("ripple");
+
+        const rect = this.getBoundingClientRect();
+
+        const size = Math.max(rect.width, rect.height);
+
+        ripple.style.width = size + "px";
+        ripple.style.height = size + "px";
+
+        ripple.style.left =
+            (e.clientX - rect.left - size / 2) + "px";
+
+        ripple.style.top =
+            (e.clientY - rect.top - size / 2) + "px";
+
+        this.appendChild(ripple);
+
+        ripple.addEventListener("animationend", () => {
+
+            ripple.remove();
+
+        });
+
+    });
+
+});
